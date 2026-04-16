5815ee832d496b43b532cc7f58cee9c06f5a1d7c
hongtao_luo <luohongtao621@hotmail.com>
2026-04-16 17:07:24 +0800

    code

--- RapidOCRSharpOnnx/Inference/PPOCR-CLS/TextClassifier.cs
+++ RapidOCRSharpOnnx/Inference/PPOCR-CLS/TextClassifier.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RapidOCRSharpOnnx.Inference.PPOCR_CLS
+namespace RapidOCRSharpOnnx.Inference.PPOCR_Cls
 {
     internal class TextClassifier
     {
