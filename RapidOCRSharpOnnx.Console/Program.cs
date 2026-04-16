c609cfe166f29fbd48a5c15c45376bc9395cbf38
hongtao_luo <luohongtao621@hotmail.com>
2026-04-16 11:34:08 +0800

    rename

---
+++ RapidOCRSharpOnnx.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using OpenCvSharp;
+using RadpidOCRCSharpOnnx.Config;
+using RadpidOCRCSharpOnnx.InferenceEngine;
+using RadpidOCRCSharpOnnx.Utils;
+using System.Runtime.InteropServices;
+namespace RadpidOCRCSharpOnnx.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var buildNumber = Environment.OSVersion.Version.Build;
+            //var dd= NativeMethods.core_getVersionRevision
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}
